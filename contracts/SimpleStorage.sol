566a04c87a5c6e8b5e7476186438d21bd25aa4af
milosdjurica <milosdjurica13@gmail.com>
2023-12-04 12:10:05 +0100

    prettier setup

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,29 +3,29 @@
 pragma solidity ^0.8.19; // solidity version
 
 contract SimpleStorage {
-    uint public myFavoriteNumber;
-    // type Person
-    struct Person {
-        uint256 favoriteNumber;
-        string name;
-    }
-    // dynamic array
-    Person[] public listOfPeople;
-    // name -> favNum
-    mapping(string => uint256) public nameToFavoriteNumber;
+	uint public myFavoriteNumber;
+	// type Person
+	struct Person {
+		uint256 favoriteNumber;
+		string name;
+	}
+	// dynamic array
+	Person[] public listOfPeople;
+	// name -> favNum
+	mapping(string => uint256) public nameToFavoriteNumber;
 
-    function store(uint256 _favoriteNumber) public virtual {
-        myFavoriteNumber = _favoriteNumber;
-    }
+	function store(uint256 _favoriteNumber) public virtual {
+		myFavoriteNumber = _favoriteNumber;
+	}
 
-    // view, pure -> view means we are just reading state , pure means cant change AND also cant view
-    function retreive() public view returns (uint256) {
-        return myFavoriteNumber;
-    }
+	// view, pure -> view means we are just reading state , pure means cant change AND also cant view
+	function retreive() public view returns (uint256) {
+		return myFavoriteNumber;
+	}
 
-    // calldata, memory, storage
-    function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        listOfPeople.push(Person(_favoriteNumber, _name));
-        nameToFavoriteNumber[_name] = _favoriteNumber;
-    }
+	// calldata, memory, storage
+	function addPerson(string memory _name, uint256 _favoriteNumber) public {
+		listOfPeople.push(Person(_favoriteNumber, _name));
+		nameToFavoriteNumber[_name] = _favoriteNumber;
+	}
 }
