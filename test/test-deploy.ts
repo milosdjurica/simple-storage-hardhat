2becddf98918858cfd195b414de70b4629799178
milosdjurica <milosdjurica13@gmail.com>
2023-12-04 13:56:03 +0100

    testing for updating store

--- test/test-deploy.ts
+++ test/test-deploy.ts
@@ -19,4 +19,14 @@ describe("SimpleStorage", function () {
 		const expectedValue = "0";
 		assert.equal(currentValue.toString(), expectedValue);
 	});
+
+	it("Should update when we call store", async function () {
+		const expectedValue = "11";
+		const transactionResponse = await simpleStorage.store(expectedValue);
+		await transactionResponse.wait(1);
+
+		const currentValue = await simpleStorage.retrieve();
+
+		assert.equal(expectedValue, currentValue.toString());
+	});
 });
